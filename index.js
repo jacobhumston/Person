d3cb82879642703c4af5e96ccab44fb2b093324c
Jacob Humston <humstonjacob@gmail.com>
2022-05-17 19:15:48 -0400

    Finished friend and removefriend methods and events.

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 class Person {
     Name;
     Age;
-    Friends;
+    #Friends;
+    #ID;
+    #isPerson;
     #createdAt;
-    Events = ["friendAdded"];
+    #Events = ["friendAdded", "friendRemoved"];
+
     constructor(Name, Age) {
         this.Name = Name ?? "No Name";
         this.Age = Age ?? 0;
-        this.Friends = [];
+        this.#Friends = [];
+        this.#ID = "ID-" + Date.now();
+        this.#isPerson = true;
         this.#createdAt = new Date(Date.now());
-        const NewEvents = {};
-        for (const Event of this.Events) {
+        for (const Event of this.#Events) {
             const Functions = [];
-            NewEvents[Event] = {
+            this[Event] = {
                 Functions: Functions,
-                Connect: function (Function) {
+                Connect: function (Function, isAsync) {
                     const Identification = Date.now();
                     Functions.push({
                         Function: Function,
+                        isAsync: isAsync ?? false,
                         Identification: Identification,
                     });
                     return {
@@ -34,34 +39,120 @@ class Person {
                 Fire: function (...Values) {
                     for (const FunctionObject of Functions) {
                         const Function = FunctionObject.Function;
-                        Function(Values);
+                        const isAsync = FunctionObject.isAsync;
+                        if (isAsync == true) {
+                            (async () => {
+                                await Function(Values);
+                            })();
+                        } else if (isAsync == false) {
+                            Function(Values);
+                        }
                     }
                 },
             };
         }
-        this.Events = NewEvents;
     }
+
+    get Friends() {
+        const ShallowCopy = [];
+        for (const Friend of this.#Friends) {
+            ShallowCopy.push(Friend);
+        }
+        return ShallowCopy;
+    }
+
+    get ID() {
+        return this.#ID;
+    }
+
+    get isPerson() {
+        return this.#isPerson;
+    }
+
     get createdAt() {
         return this.#createdAt;
     }
-    addFriend(Person) {
-        this.Friends.push(Person);
-        this.Events.friendAdded.Fire(Person);
-        return this.Friends;
+
+    get eventList() {
+        return this.#Events;
+    }
+
+    #validatePerson(Person, Context) {
+        let isPerson = false;
+        if ((typeof Person == "object") == true) {
+            if (Person.isPerson == true) {
+                isPerson = true;
+            }
+        }
+        if (isPerson == false) {
+            throw "Argument provided is not a person. Context: '" + Context + "' Type provided was: '" + typeof Person + "'";
+        }
+    }
+
+    #getContext(Method) {
+        return "Person: " + this + ", Method: " + Method;
+    }
+
+    addFriend(Person, dontThrowIfAlreadyFriends) {
+        dontThrowIfAlreadyFriends = dontThrowIfAlreadyFriends ?? false;
+        this.#validatePerson(Person, this.#getContext("addFriend"));
+        if (Person == this) throw this + " can't be friends with themself.";
+        if (this.#Friends.findIndex((Friend) => Person.ID == Friend.ID) != -1) {
+            if (dontThrowIfAlreadyFriends == false) throw this + " is already friends with " + Person + ".";
+            return;
+        }
+        this.#Friends.push(Person);
+        this.friendAdded.Fire(Person);
+        Person.addFriend(this, true);
+        return this.#Friends;
+    }
+
+    removeFriend(Person, dontThrowIfNotFriends) {
+        dontThrowIfNotFriends = dontThrowIfNotFriends ?? false;
+        this.#validatePerson(Person, this.#getContext("addFriend"));
+        const Index = this.#Friends.findIndex((Friend) => Person.ID == Friend.ID);
+        if (Index != -1) {
+            this.#Friends.splice(Index);
+        } else {
+            if (dontThrowIfNotFriends == true) throw "These two people are not friends.";
+            return;
+        }
+        this.friendRemoved.Fire(Person);
+        Person.removeFriend(this);
+        return this.#Friends;
+    }
+
+    toString() {
+        return this.Name;
+    }
+
+    Log() {
+        console.log(this);
     }
 }
 
-const Bob = new Person("Bob", 23);
-console.log(Bob.createdAt);
+const Bob = new Person("Bob", 10);
+const Jimmy = new Person("Jimmy", 10);
 
-const Event = Bob.Events.friendAdded.Connect(function (value) {
-    const Friend = value[0];
-    console.log("Added friend: ", Friend.Name);
+Bob.friendRemoved.Connect(function (Values) {
+    const Friend = Values[0];
+    console.log("Bob has lost a friend: " + Friend);
 });
 
-const Jimmy = new Person("Jimmy", 2000);
+Bob.friendAdded.Connect(function (Values) {
+    const Friend = Values[0];
+    console.log("Bob has gained a friend: " + Friend);
+});
+
+Jimmy.friendRemoved.Connect(function (Values) {
+    const Friend = Values[0];
+    console.log("Jimmy has lost a friend: " + Friend);
+});
+
+Jimmy.friendAdded.Connect(function (Values) {
+    const Friend = Values[0];
+    console.log("Jimmy has gained a friend: " + Friend);
+});
 
 Bob.addFriend(Jimmy);
-Bob.addFriend(Bob);
-
-Event.Disconnect();
+Bob.removeFriend(Jimmy);
