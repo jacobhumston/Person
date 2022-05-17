6929b5b601193e833b0b6f632b245a1b265fe61d
Jacob Humston <humstonjacob@gmail.com>
2022-05-17 15:06:11 -0400

    Fixed events.

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
 class Person {
-    Name
-    Age
-    Friends
-    #createdAt
-    Events = ["friendAdded"]
+    Name;
+    Age;
+    Friends;
+    #createdAt;
+    Events = ["friendAdded"];
     constructor(Name, Age) {
-        this.Name = Name ?? "No Name"
-        this.Age = Age ?? 0
-        this.Friends = []
-        this.#createdAt = new Date(Date.now())
-        const NewEvents = {}
-        for (const Event of Events) {
+        this.Name = Name ?? "No Name";
+        this.Age = Age ?? 0;
+        this.Friends = [];
+        this.#createdAt = new Date(Date.now());
+        const NewEvents = {};
+        for (const Event of this.Events) {
+            const Functions = [];
             NewEvents[Event] = {
-                Functions = [],
-                Connect = function(Function) {
-                    const Identification = Date.now()
+                Functions: Functions,
+                Connect: function (Function) {
+                    const Identification = Date.now();
                     Functions.push({
-                        Function = Function,
-                        Identification = Identification,
-                    })
+                        Function: Function,
+                        Identification: Identification,
+                    });
                     return {
-                        Disconnect = function() {
-                            const Index = Functions.findIndex(Event => Event.Identification == Identification)
+                        Disconnect: function () {
+                            const Index = Functions.findIndex((Event) => Event.Identification == Identification);
                             if (Index != -1) {
-                                Functions.splice(Index)
+                                Functions.splice(Index);
                             } else {
-                                throw "Event was already disconnected or removed."
+                                throw "Event was already disconnected or removed.";
                             }
-                        } 
+                        },
+                    };
+                },
+                Fire: function (...Values) {
+                    for (const FunctionObject of Functions) {
+                        const Function = FunctionObject.Function;
+                        Function(Values);
                     }
                 },
-                Fire = function(...Values) {
-                    for (const FunctionObject of Functions) {
-                        const Function = FunctionObject.Function
-                        Function(Values)
-                    }
-                }
-            }
+            };
         }
-        this.Events = NewEvents
+        this.Events = NewEvents;
     }
     addFriend(Person) {
-        this.Friends.push(Person)
-        return this.Friends
+        this.Friends.push(Person);
+        this.Events.friendAdded.Fire(Person)
+        return this.Friends;
     }
 }
 
+const Bob = new Person("Bob", 23)
+
+Bob.Events.friendAdded.Connect(function(value){
+    const Friend = value[0]
+    console.log("Added friend: ", Friend.Name)
+})
+
+const Jimmy = new Person("Jimmy", 2000)
+
+Bob.addFriend(Jimmy)
+Bob.addFriend(Bob)
